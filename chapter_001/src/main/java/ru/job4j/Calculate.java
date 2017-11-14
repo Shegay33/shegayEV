930b648e29c4f53e4caccbc50cb971be4995b8d2
shegay33 <terrasan30@gmail.com>
2017-11-14 15:49:24 +1000

    1.4 JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,18 @@
 package shegay;
 
+/**
+* Тестовый документ 
+* Для класса Calculate
+*/
+
 public class Calculate {
+	
 	public static void main(String[] args ) {
 		System.out.println("Hello world.");
 	}
+	
+	public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+    }
 	
 }
