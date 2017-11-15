03de0f05a6c4af7525e058162b0dce365edbd041
shegay33 <terrasan30@gmail.com>
2017-11-15 22:05:49 +1000

    1.4 JUnit [4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,18 +1,31 @@
-package shegay;
+package ru.job4j;
 
 /**
-* Тестовый документ 
-* Для класса Calculate
-*/
+ * Calculate class.
+ *@author Evgeniy Shegay (terrasan30@gmail.com)
+ *since 15.11.2017
+ */
+
 
 public class Calculate {
-	
-	public static void main(String[] args ) {
-		System.out.println("Hello world.");
-	}
-	
-	public String echo(String name) {
-    return "Echo, echo, echo : " + name;
+
+   /**
+    * Main.
+	* @param args - args.
+	*/
+
+   public static void main(String[] args) {
+       System.out.println("Hello World.");
+   }
+   
+/**
+ * Method echo.
+ * @param name Your name.
+ * @return Echo plus your name.
+ */
+ 
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
-	
+
 }
